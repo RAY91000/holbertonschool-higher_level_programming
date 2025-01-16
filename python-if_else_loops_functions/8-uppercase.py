700401318fcaa1b2d532b584c95b3291b0bb04d1
root <ray.dotropg@gmail.com>
2025-01-16 15:00:16 +0000

    8-uppercase.py

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/bin/bash/python3
+def uppercase(str):
+    for char in str:
+        if ord('a') <= ord(char) <= ord('z'):
+            print(chr(ord(char) - 32), end="")
+        else:
+            print(char, end="")
+    print()
+
