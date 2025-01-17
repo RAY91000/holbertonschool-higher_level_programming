4ed045861a4f36c0f68d3a982feb06f62a99afcf
root <ray.dotropg@gmail.com>
2025-01-17 16:26:04 +0000

    corection of task 0

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    from add_0 import add
-
-    a = 1
-    b = 2
-    print(f"{a} + {b} = {add(a, b)}")
+import random
+number = random.randint(-10, 10)
+if number > 0:
+    print(f"{number} is positive")
+elif number == 0:
+    print(f"{number} is zero")
+else:
+    print(f"{number} is negative")
