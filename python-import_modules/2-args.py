f1c925f2460e817a98475b208c8072553dd32c95
root <ray.dotropg@gmail.com>
2025-01-17 16:32:15 +0000

    2-args.py

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+    argv = sys.argv[1:]  # Exclude the script name
+    argc = len(argv)
+
+    if argc == 0:
+        print("0 arguments.")
+    else:
+        print(f"{argc} argument{'s' if argc > 1 else ''}:")
+        for i, arg in enumerate(argv, start=1):
+            print(f"{i}: {arg}")
