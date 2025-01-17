d9aed8155db4bade89596ad13b78b4b5ffaf7afc
root <ray.dotropg@gmail.com>
2025-01-17 16:08:01 +0000

    trying debbuging task 9

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    last_digit = abs(number) % 10  # Get the absolute value to handle negative numbers
-    print(last_digit, end="")     # Print the last digit without a newline
+    last_digit = abs(number) % 10
+    print(last_digit, end="")
     return last_digit
