a2dad8b8f9437bff6439e85e0fea64c705c204a8
root <ray.dotropg@gmail.com>
2025-01-23 09:36:10 +0000

    trying debbuging task 2 again

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
-    for i in range(min(x, len(my_list))):
+    for i in range(x):
         try:
             print("{:d}".format(my_list[i]), end="")
             count += 1
         except (ValueError, TypeError):
             pass
+        except IndexError:
+            raise IndexError("Index out of range")
     print()
     return count
 
