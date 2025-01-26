9b9c4ad50bfb61f0f13dc16979bbff56ace78b05
root <ray.dotropg@gmail.com>
2025-01-26 21:26:56 +0000

    correction of task 4

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -22,10 +22,11 @@ def print_square(size):
         TypeError: If size is not an integer.
         ValueError: If size is less than 0.
     """
-    if not isinstance(size, int):
+    if size is None or not isinstance(size, int):
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
 
     for _ in range(size):
         print("#" * size)
+
