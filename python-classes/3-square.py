0d8e7e8876c506df8e522d280abd0b770c5ba991
root <ray.dotropg@gmail.com>
2025-01-29 11:35:46 +0000

    3-square.py

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -18,6 +18,20 @@ area():
 
 
 class Square:
+     """
+        Initializes a square with a given size.
+
+        Parameters:
+        -----------
+        size : int, optional
+            The size of the square's side (default is 0).
+
+        Raises:
+        -------
+        TypeError
+            If size is not an integer.
+        ValueError
+            If size is negative.
     def __init__(self, size=0):
         """
         Initializes a square with a given size.
