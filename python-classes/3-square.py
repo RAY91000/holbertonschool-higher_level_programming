42498430e2edbf9cc11063e8dd5c85ec6f6200f4
root <ray.dotropg@gmail.com>
2025-01-29 11:39:02 +0000

    3-square.py

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -2,18 +2,6 @@
 """
 A class to represent a square.
 
-Attributes:
------------
-__size : int
-    The size of the square's side (private attribute).
-
-Methods:
---------
-__init__(size=0):
-    Initializes a square with a given size.
-
-area():
-    Returns the area of the square.
 """
 
 
@@ -39,28 +27,4 @@ class Square:
         Parameters:
         -----------
         size : int, optional
-            The size of the square's side (default is 0).
-
-        Raises:
-        -------
-        TypeError
-            If size is not an integer.
-        ValueError
-            If size is negative.
-        """
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
-
-    def area(self):
-        """
-        Calculates and returns the area of the square.
-
-        Returns:
-        --------
-        int
-            The area of the square.
-        """
-        return self.__size * self.__size
+            The size of the square
