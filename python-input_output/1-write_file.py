47e0148a0a42b5b8de08a9780f8f8d8766890d57
root <ray.dotropg@gmail.com>
2025-02-11 11:31:54 +0000

    fastcomm

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Function write a string to textfile and return the number of character written.
+
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    Write string to UTF-8 textfile and return the number of character written.
+
+    Args:
+        filename (str): The name of the file to write to.
+        Defaults to an empty string.
+        text (str): The text to write to the file. Defaults to an empty string.
+
+    Returns:
+        int: The number of characters written.
+    """
+    with open(filename, "w", encoding="utf-8") as file:
+        return file.write(text)
---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Function to create an object from a JSON file."""
+
+
+import json
+
+
+def load_from_json_file(filename):
+    """Creates a Python object from a JSON file.
+
+    Args:
+        filename (str): The name of the file to read from.
+
+    Returns:
+        object: The Python object represented by the JSON file.
+    """
+    with open(filename, "r", encoding="utf-8") as file:
+        return json.load(file)
