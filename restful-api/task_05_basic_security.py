f557c25a257fcef41956af75b38d08deaab3121c
ray91000 <ray.dotropg@gmail.com>
2025-02-20 01:36:53 +0100

    task 5

--- restful-api/task_05_basic_security.py
+++ restful-api/task_05_basic_security.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 from flask import Flask, jsonify, request
 from flask_httpauth import HTTPBasicAuth
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
